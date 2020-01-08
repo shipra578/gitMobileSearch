2093daa4a729fc017a3684d2fb5756c93b90d1e8
Shipra Singh <shipra.singh@medlife.com>
2020-01-08 18:44:03 +0530

    created repo detail screen

---
+++ app/src/main/java/com/shipra/android/gitmobilesearch/ui/RepoDetailActivity.java
@@ -0,0 +1,16 @@
+package com.shipra.android.gitmobilesearch.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shipra.android.gitmobilesearch.R;
+
+public class RepoDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repo_detail);
+    }
+}
